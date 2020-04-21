4433876f2d4a346ef06bc930d6b422eb13c37222
Abdurrahman Faiz <45443640+bigshifu@users.noreply.github.com>
2020-04-21 17:28:53 +0700

    Update connect.php

--- Support/action/connect.php
+++ Support/action/connect.php
@@ -8,8 +8,6 @@ $DB_PWD  = "57b9b17c";
 $DB_NAME = "heroku_946f48f4a9d8130";        
 
 $con = mysqli_connect($IP, $DB_USER, $DB_PWD, $DB_NAME);
-// mysql_query("set names 'utf8'",$con);
-// $dbLink = mysql_select_db($DB_NAME,$con);
 
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
